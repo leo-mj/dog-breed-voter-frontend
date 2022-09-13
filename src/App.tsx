a66099473ec064e36fa83c3d47db5b98b5d7001d
Leo <leo.maedje@gmail.com>
2022-09-13 13:35:07 +0200

    made voting tool functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { VotingTool } from "./components/VotingTool";
 
 function App(): JSX.Element {
+  const [votesRegistered, setVotesRegistered] = useState<number>(0);
   return (
     <>
-      <VotingTool />
+      <VotingTool
+        votesRegistered={votesRegistered}
+        setVotesRegistered={setVotesRegistered}
+      />
       <footer>
         <a href="https://dog.ceo/dog-api/">Thanks to Dog API </a>
       </footer>
