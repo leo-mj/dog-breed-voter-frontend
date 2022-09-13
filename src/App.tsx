3664dcd5b9fd4c3c02bc1ecde93a29661c07c082
purpurlu <laurazurbayeva@gmail.com>
2022-09-13 12:41:55 +0100

    add top-ten leaderboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { VotingTool } from "./components/VotingTool";
+import { TopTen } from "./components/TopTen";
 
 function App(): JSX.Element {
   return (
     <>
       <VotingTool />
+      <TopTen />
       <footer>
         <a href="https://dog.ceo/dog-api/">Thanks to Dog API </a>
       </footer>
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -7,3 +7,9 @@ export interface IDogAPIResponse {
   message: string;
   status: string;
 }
+
+export interface IDataTopTen {
+  breed_id: number;
+  dog_breed: string;
+  votes: number;
+}
